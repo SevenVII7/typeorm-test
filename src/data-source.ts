7599ee49a69bcb8842cf75f1cd729472982d1ad6
leon <leon.liu@accuhit.net>
2024-03-01 18:28:43 +0800

    更新

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from './entity/User'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,15 +10,15 @@ const dbPassword = process.env.DB_PASSWORD
 const database = process.env.DATABASE
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: dbHost,
-    port: 3306,
-    username: dbUser,
-    password: dbPassword,
-    database: database,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+  type: 'mysql',
+  host: dbHost,
+  port: 3306,
+  username: dbUser,
+  password: dbPassword,
+  database: database,
+  // synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: []
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,46 @@
-// import { AppDataSource } from "./data-source"
+import express from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import { AppDataSource } from './data-source'
+import { User } from './entity/User'
+import type { Response, Request } from 'express'
+
+dotenv.config()
+
+const app = express()
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.post('/user', async (req: Request, res: Response) => {
+  try {
+    console.log(req.body)
+
+    const user = new User()
+    console.log(user)
+
+    user.firstName = req.body.firstName
+    user.lastName = req.body.lastName
+    user.age = req.body.age
+    console.log(user)
+
+    await AppDataSource.manager.save(user)
+    console.log(user)
+
+    res.status(200).json(`{"msg": "Saved a new user with id: ${user.id}"}`)
+  } catch {
+    res.status(400).json('failed')
+  }
+})
+;(async () => {
+  await AppDataSource.initialize()
+  app.listen(process.env.PORT, () => {
+    console.log('Express server listening on Port ', process.env.PORT)
+  })
+})()
+
 // import { User } from "./entity/User"
 
 // AppDataSource.initialize().then(async () => {
@@ -18,16 +60,3 @@
 //     console.log("Here you can setup and run express / fastify / any other framework.")
 
 // }).catch(error => console.log(error))
-
-import express from 'express'
-const path = require('path')
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-const app = express()
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.listen(process.env.PORT, () => {
-    console.log('Express server listening on Port ', process.env.PORT)
-})
-
